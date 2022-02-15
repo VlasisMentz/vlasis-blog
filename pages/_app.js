6a69d33e75944506c182422626cccfdc2ecd3d6f
Vlasis Mentzelopoulos <vlasism@gmail.com>
2022-02-15 16:02:09 +0200

    add components

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import '../styles/globals.scss'
+import React, { useEffect, useState } from 'react'
+import { Layout } from '../components'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
