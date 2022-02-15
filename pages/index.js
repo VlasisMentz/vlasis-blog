611ffb4c026aabbe6bad91ee6b652ad121e770b0
Vlasis Mentzelopoulos <vlasism@gmail.com>
2022-02-15 17:48:38 +0200

    add postCard

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12 ">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="lg: col-span-4 col-span-1">
@@ -28,7 +28,7 @@ export default function Home({ posts }) {
 
 export async function getStaticProps() {
   const posts = (await getPosts()) || []
-
+  console.log(posts)
   return {
     props: { posts },
   }
