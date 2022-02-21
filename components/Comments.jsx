edc2b180c22c593f538c76633d4bab85e3bafd49
Vlasis Mentz <vlasism@gmail.com>
2022-02-21 21:08:49 +0200

    fix comments component

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import parse from 'html-react-parser'
+import { getComments } from '../services'
 
-const Comments = () => {
-  return <div>Comments</div>
+const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+    getComments(slug).then((result) => setComments(result))
+    console.log(comments)
+  }, [])
+
+  return (
+    <>
+      {comments.map((comment) => (
+        <h1>{comment.createdAt}</h1>
+      ))}
+    </>
+  )
 }
 
 export default Comments
