491127c6948e90bf2b74fe1182432a294d310a69
Vlasis Mentz <vlasism@gmail.com>
2022-02-23 19:48:17 +0200

    change neme

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Blase Blog
+              To Eida
             </span>
           </Link>
         </div>
